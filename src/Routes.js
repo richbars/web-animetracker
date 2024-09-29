e7fc4d56a649401df26f7beaadaccd4a26e4f4ea
richbars <richard.santos21@hotmail.com>
2024-09-29 20:18:12 -0300

    .

--- src/Routes.js
+++ src/Routes.js
@@ -3,12 +3,14 @@ import BuscarCEP from "./buscarcep/buscarcep";
 import AnimeTracker from "./animetracker/animetracker";
 import App from "./App";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Details from "./animetracker/details";
 
 const AppRoutes = () => {
     return (
         <Router>
             <Routes>
                 <Route path="/" element={<App />} />
+                <Route path="/:type/:id" element={<Details />} />
                 <Route path="/animetracker" element={<AnimeTracker />} />
                 <Route path="/buscarcep" element={<BuscarCEP />} />
             </Routes>
--- src/animetracker/animetracker.js
+++ src/animetracker/animetracker.js
@@ -1,12 +1,11 @@
-import { FiSearch } from 'react-icons/fi'
+import { FiSearch } from 'react-icons/fi';
 import '../animetracker/style.css';
 import { useState } from 'react';
 import { getMangaByName, getAnimeByName } from '../services/jikan';
 
 function AnimeTracker() {
-
     const [input, setInput] = useState('');
-    const [response, setResponse] = useState(''); // Inicialize como null
+    const [response, setResponse] = useState('');
     const [type, setType] = useState('manga');
 
     const handleSearch = async () => {
@@ -23,22 +22,17 @@ function AnimeTracker() {
             alert("Error: " + error);
         }
     };
-    
 
-    
     const handleKeyDown = (event) => {
         if (event.key === 'Enter') {
-        handleSearch();
+            handleSearch();
         }
     };
 
     return (
-
         <div className='container'>
-            
             <div className="containerInput">
-
-            <select 
+                <select 
                     value={type} 
                     onChange={(event) => setType(event.target.value)}
                     className="type-selector"
@@ -48,39 +42,42 @@ function AnimeTracker() {
                 </select>
 
                 <input
-                type="text"
-                placeholder="Pesquise..."
-                value={input}
-                onChange={(event) => setInput(event.target.value)}
-                onKeyDown={handleKeyDown}
+                    type="text"
+                    placeholder="Pesquise..."
+                    value={input}
+                    onChange={(event) => setInput(event.target.value)}
+                    onKeyDown={handleKeyDown}
                 />
 
                 <button className="buttonSearch" onClick={handleSearch}>
-                    <FiSearch size={25} color='#fff'/>
+                    <FiSearch size={25} color='#fff' />
                 </button>
-                
             </div>
 
             {response && response.data && response.data.length > 0 ? (
                 <div className='animelist'>
                     {response.data.map((manga) => (
                         <div key={manga.mal_id} className="manga-item">
-                        <img src={manga.images.jpg.image_url} alt={manga.title_english} />
-                        <h3 className="manga-title">English: {manga.title_english}</h3>
-                        <br /> {/* Quebra de linha */}
-                        <h3 className="manga-title">Original: {manga.title}</h3>
-                        <br /> {/* Quebra de linha */}
-                        <h3 className="manga-title">ID: {manga.mal_id}</h3>
-                    </div>
 
+                            <a href={`/${type}/${manga.mal_id}`} target="_blank" rel="noopener noreferrer">
+                                <img src={manga.images.jpg.image_url} alt={manga.title_english} />
+                            </a>
+
+                            <h3 className="manga-title">English: {manga.title_english}</h3><br />
+
+                            
+                            <h3 className="manga-title">Original: {manga.title}</h3><br />
+                            
+                            <h3 className="manga-title">ID: {manga.mal_id}</h3>
+                            
+                        </div>
                     ))}
                 </div>
-                ) : (
-                <p>No manga found. Please search.</p>
-                )}
-            </div>
+            ) : (
+                <p>No manga or anime found. Please search.</p>
+            )}
+        </div>
     );
-  }
-  
-  export default AnimeTracker;
-  
+}
+
+export default AnimeTracker;
